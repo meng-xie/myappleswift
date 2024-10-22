5681790522b4f9ecc91b60466bb2adec88896812
Meng Xie <myxj128@gmail.com>
2024-10-22 13:19:53 -0700

    Initial Commit

---
+++ myswift/myswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  myswiftApp.swift
+//  myswift
+//
+//  Created by Meng Xie on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct myswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ myswiftTests/myswiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  myswiftTests.swift
+//  myswiftTests
+//
+//  Created by Meng Xie on 10/22/24.
+//
+
+import Testing
+
+struct myswiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
